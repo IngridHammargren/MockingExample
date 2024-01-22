e6636a532f365e65117e80b3a15fdf7c3881c29c
IngridHammargren <ingrid.hammargren@iths.se>
2024-01-22 14:27:04 +0100

    Add test for getID

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -1,7 +1,34 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 class EmployeeTest {
 
+    private Employee employee;
+
+    @BeforeEach
+    void init() { employee = new Employee("123", 26000); }
+
+    @Test
+    @DisplayName("Control that getID returns right ID ")
+    void controlThatGetIdReturnsRightId() {
+        var result = employee.getId();
+        assertThat(result).isEqualTo("123");
+    }
+
+
+        
+
+
+    
+
+
+
+        
+
 }
+
